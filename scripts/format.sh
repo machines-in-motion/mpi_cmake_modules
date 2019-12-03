63a50bc72b8a6a9b07f4a3a8dc3538508b626426
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2019-12-03 17:41:58 +0100

    Add output for successful formatting

--- scripts/format.sh
+++ scripts/format.sh
@@ -14,6 +14,7 @@ then
     echo "Error while formatting."
     exit ${clang_return_code}
 elif [ "${format_out}" = "" ]; then
+    echo "Formatting succeeded."
     exit 0
 else
     echo "Error while formatting."
