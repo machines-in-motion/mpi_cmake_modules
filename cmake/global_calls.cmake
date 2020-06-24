60d252f84392a0d58de663f4170b40de2bee1991
MaximilienNaveau <maximilien.naveau@gmail.com>
2020-06-24 14:53:07 +0200

    add a bit of doc and code formatting

--- cmake/global_calls.cmake
+++ cmake/global_calls.cmake
@@ -10,18 +10,20 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/os_detection.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/setup_xenomai.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/documentation.cmake)
 
 # defining -DXENOMAI, -DRT_PREEMPT or -DNON_REAL_TIME
 # based on 'uname -a' command
-DEFINE_OS()
+define_os()
 
 # if os is xenomami, setting up Xenomai_LIBS, calling
 # xeno-config, and adding correct directory for include
 # and linkage
 if(CURRENT_OS STREQUAL "xenomai")
-  SETUP_XENOMAI()
+  setup_xenomai()
 else()
   set(Xenomai_LIBS pthread edit curses nsl glut GL GLU X11 Xmu m)
 endif()
 
+# All package have a doc target. This target may do nothing.
 create_doc_target()
