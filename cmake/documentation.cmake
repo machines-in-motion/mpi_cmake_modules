0f3245bc15bfff46615d8996f9490c3ee2a2b9cc
MaximilienNaveau <maximilien.naveau@gmail.com>
2020-07-01 16:50:13 +0200

    preserve the backward compatibility

--- cmake/documentation.cmake
+++ cmake/documentation.cmake
@@ -31,7 +31,7 @@ macro(CREATE_DOC_TARGET)
 endmacro(CREATE_DOC_TARGET)
 
 #.rst:
-# .. cmake:command:: BUILD_DOCUMENTATION
+# .. cmake:command:: ADD_DOCUMENTATION
 #
 #   Create targets that are dependencies to the *doc* target. It builds the
 #   documentation html output using the classic Doxygen output and the rather
@@ -44,3 +44,47 @@ macro(ADD_DOCUMENTATION)
   add_sphinx_documentation()
 
 endmacro(ADD_DOCUMENTATION)
+
+#.rst:
+# 
+# .. warning:: The following is deprecated. It is used for backward support.
+#
+
+#.rst:
+# .. cmake:command:: BUILD_DOCUMENTATION
+#
+#   See :cmake:command:`ADD_DOCUMENTATION`
+#
+macro(BUILD_DOCUMENTATION)
+
+  message(WARNING "'build_documentation()' is deprecated, "
+                  "please prefer 'add_documentation()'")
+  add_documentation()
+
+endmacro(BUILD_DOCUMENTATION)
+
+#.rst:
+# .. cmake:command:: BUILD_DOXYGEN_DOCUMENTATION
+#
+#   See :cmake:command:`ADD_DOXYGEN_DOCUMENTATION`
+#
+macro(BUILD_DOXYGEN_DOCUMENTATION)
+
+  message(WARNING "'BUILD_DOXYGEN_DOCUMENTATION()' is deprecated, "
+                  "please prefer 'add_doxygen_documentation()'")
+  add_doxygen_documentation()
+
+endmacro(BUILD_DOXYGEN_DOCUMENTATION)
+
+#.rst:
+# .. cmake:command:: BUILD_SPHINX_DOCUMENTATION
+#
+#   See :cmake:command:`ADD_DOCUMENTATION`
+#
+macro(BUILD_SPHINX_DOCUMENTATION)
+
+  message(WARNING "'build_sphinx_documentation()' is deprecated, "
+                  "please prefer 'add_sphinx_documentation()'")
+  add_sphinx_documentation()
+
+endmacro(BUILD_SPHINX_DOCUMENTATION)
