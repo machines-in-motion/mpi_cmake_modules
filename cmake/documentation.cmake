ffed55ea3ad89be88b9480be1d55c2ba5b4584a6
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2020-10-27 10:50:32 +0100

    update warning to deprecation for the documentation

--- cmake/documentation.cmake
+++ cmake/documentation.cmake
@@ -59,7 +59,7 @@ endmacro(ADD_DOCUMENTATION)
 #
 macro(BUILD_DOCUMENTATION)
 
-  message(WARNING "'build_documentation()' is deprecated, "
+  message(DEPRECATION "'build_documentation()' is deprecated, "
                   "please prefer 'add_documentation()'")
   add_documentation()
 
@@ -72,7 +72,7 @@ endmacro(BUILD_DOCUMENTATION)
 #
 macro(BUILD_DOXYGEN_DOCUMENTATION)
 
-  message(WARNING "'BUILD_DOXYGEN_DOCUMENTATION()' is deprecated, "
+  message(DEPRECATION "'BUILD_DOXYGEN_DOCUMENTATION()' is deprecated, "
                   "please prefer 'add_doxygen_documentation()'")
   add_doxygen_documentation()
 
@@ -85,7 +85,7 @@ endmacro(BUILD_DOXYGEN_DOCUMENTATION)
 #
 macro(BUILD_SPHINX_DOCUMENTATION)
 
-  message(WARNING "'build_sphinx_documentation()' is deprecated, "
+  message(DEPRECATION "'build_sphinx_documentation()' is deprecated, "
                   "please prefer 'add_sphinx_documentation()'")
   add_sphinx_documentation()
 
