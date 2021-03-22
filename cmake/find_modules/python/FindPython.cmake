0748c5914119e34cd4113cd15d186c2bd56d627b
Maximilien Naveau <maximilien.naveau@gmail.com>
2021-03-22 18:10:53 +0100

    do not look for python if it's already found, this creates issues

--- cmake/find_modules/python/FindPython.cmake
+++ cmake/find_modules/python/FindPython.cmake
@@ -1,17 +1,43 @@
+#
+# Copyright (c) 2019, New York University and Max Planck Gesellschaft. License
+# BSD-3 clause
+#
 
-set(PYTHON_VERSION "" CACHE STRING
-  "Specify specific Python version to use ('major.minor' or 'major')")
+#
+# We priviledge Python3.
+#
+set(PYTHON_VERSION
+    ""
+    CACHE STRING
+          "Specify specific Python version to use ('major.minor' or 'major')")
 # if not specified otherwise use Python 3
 if(NOT PYTHON_VERSION)
   set(PYTHON_VERSION "3")
 endif()
 
-find_package(PythonInterp ${PYTHON_VERSION} REQUIRED)
-message(STATUS "Using PYTHON_EXECUTABLE: ${PYTHON_EXECUTABLE}")
+#
+# If the PYTHON_EXECUTABLE is already a valid path we do not look for it.
+#
+if(EXISTS ${PYTHON_EXECUTABLE})
+message(STATUS "PYTHON_EXECUTABLE already found: ${PYTHON_EXECUTABLE}")  
+else()
+  find_package(PythonInterp ${PYTHON_VERSION} REQUIRED)
+endif()
 
-find_package(PythonLibs ${PYTHON_VERSION} REQUIRED)
+#
+# If the libraries and include dirs have already been found we do not for them.
+#
+if(EXISTS ${PYTHON_LIBRARY} AND EXISTS ${PYTHON_INCLUDE_DIR})
+message(STATUS "PYTHON_INCLUDE_DIR already found: ${PYTHON_INCLUDE_DIR}")
+message(STATUS "PYTHON_LIBRARY already found: ${PYTHON_LIBRARY}")
+else()
+  message(STATUS "Trying to find the libraries corresponding to the "
+                 "PYTHON_EXECUTABLE: ${PYTHON_EXECUTABLE}")
+  find_package(PythonLibs ${PYTHON_VERSION} REQUIRED)
+endif()
 
+# We display what has been found so far.
 message(STATUS "FindPython has found:")
 message(STATUS "    - PYTHON_EXECUTABLE: ${PYTHON_EXECUTABLE}")
 message(STATUS "    - PYTHON_INCLUDE_DIR: ${PYTHON_INCLUDE_DIR}")
-message(STATUS "    - PYTHON_LIBRARIES: ${PYTHON_LIBRARIES}")
+message(STATUS "    - PYTHON_LIBRARY: ${PYTHON_LIBRARY}")
