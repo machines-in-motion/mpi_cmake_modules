7caa569f1b24097cc4fb7591186d4858650cc116
Maximilien Naveau <maximilien.naveau@gmail.com>
2021-03-23 15:27:02 +0100

    fix the get_install_dir

--- cmake/find_modules/python/FindPython.cmake
+++ cmake/find_modules/python/FindPython.cmake
@@ -19,21 +19,29 @@ endif()
 # If the PYTHON_EXECUTABLE is already a valid path we do not look for it.
 #
 if(EXISTS ${PYTHON_EXECUTABLE})
-message(STATUS "PYTHON_EXECUTABLE already found: ${PYTHON_EXECUTABLE}")  
+  message(STATUS "PYTHON_EXECUTABLE already found: ${PYTHON_EXECUTABLE}")
 else()
-  find_package(PythonInterp ${PYTHON_VERSION} REQUIRED)
+  if(${Python_FIND_REQUIRED})
+    find_package(PythonInterp ${PYTHON_VERSION} REQUIRED)
+  else()
+    find_package(PythonInterp ${PYTHON_VERSION} QUIET)
+  endif()
 endif()
 
 #
 # If the libraries and include dirs have already been found we do not for them.
 #
-if(EXISTS ${PYTHON_LIBRARY} AND EXISTS ${PYTHON_INCLUDE_DIR})
-message(STATUS "PYTHON_INCLUDE_DIR already found: ${PYTHON_INCLUDE_DIR}")
-message(STATUS "PYTHON_LIBRARY already found: ${PYTHON_LIBRARY}")
+if(EXISTS "${PYTHON_LIBRARY}" AND EXISTS "${PYTHON_INCLUDE_DIR}")
+  message(STATUS "PYTHON_INCLUDE_DIR already found: ${PYTHON_INCLUDE_DIR}")
+  message(STATUS "PYTHON_LIBRARY already found: ${PYTHON_LIBRARY}")
 else()
   message(STATUS "Trying to find the libraries corresponding to the "
                  "PYTHON_EXECUTABLE: ${PYTHON_EXECUTABLE}")
-  find_package(PythonLibs ${PYTHON_VERSION} REQUIRED)
+  if(${Python_FIND_REQUIRED})
+    find_package(PythonLibs ${PYTHON_VERSION} REQUIRED)
+  else()
+    find_package(PythonLibs ${PYTHON_VERSION} QUIET)
+  endif()
 endif()
 
 # We display what has been found so far.
--- cmake/get_python_install_dir.cmake
+++ cmake/get_python_install_dir.cmake
@@ -16,19 +16,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/get_python_interpreter.cmake)
 function(get_python_install_dir output)
 
   # Find the python interpreter.
-  if(NOT PYTHON_EXECUTABLE)
-    find_program(PYTHON_EXECUTABLE "python")
-  endif()
-
-  set(PYTHON_VERSION
-      ""
-      CACHE STRING
-            "Specify specific Python version to use ('major.minor' or 'major')")
-  # if not specified otherwise use Python 3
-  if(NOT PYTHON_VERSION)
-    set(PYTHON_VERSION "3")
-  endif()
-  message(STATUS "Using PYTHON_EXECUTABLE: ${PYTHON_EXECUTABLE}")
+  find_package(Python REQUIRED)
 
   # code to find installation path for python libs
   set(_python_code
