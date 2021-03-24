b253c2a5db3df29c1310f25df3981851775525ac
Maximilien Naveau <maximilien.naveau@gmail.com>
2021-03-24 16:43:43 +0100

    simplify setup.py of demos

--- demos/example_python_package/setup.py
+++ demos/example_python_package/setup.py
@@ -23,73 +23,6 @@ THIS_PACKAGE_PYTHON_MODULES = ["example_python_package"]
 THIS_PACKAGE_VERSION = "1.0"
 
 
-def _get_build_dir():
-    """get where the build dir is:
-
-    Returns:
-        str: Path to the build directory
-    """
-    build_folder = "build"
-
-    if "--build-base" in sys.argv:
-        index = sys.argv.index("--build-base")
-    elif "-b" in sys.argv:
-        index = sys.argv.index("-b")
-    else:
-        index = None
-    if index is not None:
-        if index + 1 < len(sys.argv):
-            build_folder = sys.argv[index + 1]
-
-    return Path(build_folder)
-
-
-def _get_install_dir():
-    """get where the build dir is:
-
-    Returns:
-        str: Path to the install directory
-    """
-    install_folder = "install"
-
-    if "--prefix" in sys.argv:
-        index = sys.argv.index("--build-base")
-    else:
-        index = None
-    if index is not None:
-        if index + 1 < len(sys.argv):
-            install_folder = sys.argv[index + 1]
-
-    return Path(install_folder)
-
-
-def _print_error(*args, **kwargs):
-    """ Print in stderr. """
-    print(*args, file=sys.stderr, **kwargs)
-
-
-def _custom_install(install_object):
-    """
-    Build the doc in build_folder/doc/html
-    Install the doc in install_folder/share/<project_name>/dos/sphinx/html.
-    """
-
-    # destination = (
-    #     Path(_get_install_dir())
-    #     / "share"
-    #     / THIS_PACKAGE_NAME
-    #     / "docs"
-    #     / "sphinx"
-    #     / "html"
-    # )
-    # source = Path("doc") / "html"
-    # # clean the install tree
-    # rmtree("share", ignore_errors=True)
-    # destination.mkdir(parents=True, exist_ok=True)
-    # copytree(source, destination)
-    pass
-
-
 class custom_build_py(build_py):
     def run(self):
 
@@ -116,24 +49,6 @@ class custom_build_py(build_py):
         build_py.run(self)
 
 
-class custom_install(install):
-    def run(self):
-        install.run(self)
-        _custom_install(self)
-
-
-class custom_develop(develop):
-    def run(self):
-        develop.run(self)
-        _custom_install(self)
-
-
-class custom_egg_info(egg_info):
-    def run(self):
-        egg_info.run(self)
-        _custom_install(self)
-
-
 # Setup this package.
 setup(
     # Standard metadata.
@@ -148,10 +63,5 @@ setup(
     },
     packages=[THIS_PACKAGE_NAME],
     # Here we call the above class upon build.
-    cmdclass={
-        "build_py": custom_build_py,
-        "install": custom_install,
-        "develop": custom_develop,
-        "egg_info": custom_egg_info,
-    },
+    cmdclass={"build_py": custom_build_py},
 )
