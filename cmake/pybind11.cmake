7697debb032caa67a017f58975de4be9c6704dfc
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-11-28 17:57:29 +0100

    Add NO_EXTRAS option to add_pybind11_module()
    This disables some linker optimisations in pybind11.  Normally one would
    not want to disable them but there are unresolved cases where the
    optimisation crashes gcc with an internal compiler error...

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -13,20 +13,30 @@
 #    match the name of the Python module!) followed by a list of source files.
 #
 #    Optional arguments:
+#     - NO_EXTRAS:  Disables some optimisation in pybind11 (see documentation of
+#         pybind11_add_module for details).
 #     - LINK_LIBRARIES:  List of libraries that are linked to the target.
 #     - INLUCDE_DIRS:  List of include directories.  "include" is added by
 #       default.
 #
 macro(add_pybind11_module module_name)
     cmake_parse_arguments(ADD_PYBIND11_MODULE
-        ""  # options without arguments
+        "NO_EXTRAS"  # options without arguments
         ""  # options with single argument
         "INCLUDE_DIRS;LINK_LIBRARIES"  # options with multiple arguments
         ${ARGN}
     )
     set(ADD_PYBIND11_MODULE_SRC ${ADD_PYBIND11_MODULE_UNPARSED_ARGUMENTS})
 
-    pybind11_add_module(${module_name} ${ADD_PYBIND11_MODULE_SRC})
+    if (ADD_PYBIND11_MODULE_NO_EXTRAS)
+        set(NO_EXTRAS "NO_EXTRAS")
+    else()
+        # make sure it is not set
+        unset(NO_EXTRAS)
+    endif()
+
+    # NO_EXTRAS
+    pybind11_add_module(${module_name} ${NO_EXTRAS} ${ADD_PYBIND11_MODULE_SRC})
     target_include_directories(${module_name} PUBLIC
         $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
         $<INSTALL_INTERFACE:include>
